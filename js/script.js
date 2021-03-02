86f2bb17544488a24ab22febff4ad2500fb710da
RolandEmojevbe <roland.emojevbe@outlook.com>
2021-03-02 18:21:26 +0000

    updates styling of holiprep project page although javascript not working

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function menuToggle() {
 //2.0
 var slideIndex = [1,1];
 /* Class the members of each slideshow group with different CSS classes */
-var slideId = ["mySlides1", "mySlides2", "mySlides3" ]
+var slideId = ["mySlides1", "mySlides2"]
 showSlides(1, 0);
 showSlides(1, 1);
 
